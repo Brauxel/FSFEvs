921d85b75ea87b2f0b7fcceab4ec75f2636c6768
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-10-03 21:58:47 +1100

    418 with x header

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.status(418).end()
-	// res.send('Hello World!');
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('Hello World!');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
