ccd2a867946f39c4101c9ec00e9aea8ec2db051c
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-10-03 23:45:19 +1100

    back to 200

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.set('X-full-stack', '4life');
-	res.status(418);
-	res.send('Hello World!');
+	res.status(200).send('Hello World!');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
